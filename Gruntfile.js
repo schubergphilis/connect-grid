01ad2da7106cfd34aee6046e123db04afab2afee
Anton Nekipelov <anton.nekipelov@gmail.com>
2014-07-15 10:46:28 +0200

    grunt release plugin (register task)

--- Gruntfile.js
+++ Gruntfile.js
@@ -71,6 +71,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-processhtml');
+    grunt.loadNpmTasks('grunt-release');
 
     // Default task.
     grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'processhtml']);
