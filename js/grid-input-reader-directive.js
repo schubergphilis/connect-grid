6e2375389b4039f621f197f8d90fba0410beb427
Anton Nekipelov <anton.nekipelov@gmail.com>
2014-08-28 16:35:52 +0200

    fix flickering when starting editing cell after scrolling the parent container

--- js/grid-input-reader-directive.js
+++ js/grid-input-reader-directive.js
@@ -7,6 +7,17 @@
             require: '?ngModel',
             link: function (scope, element/*, attrs, ngModel*/) {
                 scope.input = '';
+                scope.textAreaPosition = 'absolute';
+
+                scope.editorTopPosition = function () {
+                    var activeCellCoordinates = scope.getCellCoordinates(scope.activeCellModel.row, scope.activeCellModel.column);
+                    return activeCellCoordinates.top;
+                };
+
+                scope.editorLeftPosition = function () {
+                    var activeCellCoordinates = scope.getCellCoordinates(scope.activeCellModel.row, scope.activeCellModel.column);
+                    return activeCellCoordinates.left;
+                };
 
                 scope.$watch('input', function (newVal, oldVal) {
                     if (newVal !== oldVal && newVal.length > 0) {
@@ -31,13 +42,14 @@
                 }, true);
 
                 var select = function () {
-                    element.find('textarea')[0].value = scope.renderCellContent(scope.activeCellModel.row, scope.activeCellModel.column);
-                    element.find('textarea')[0].select();
+                    var textareaEl = element.find('textarea')[0];
+                    textareaEl.value = scope.renderCellContent(scope.activeCellModel.row, scope.activeCellModel.column);
+                    textareaEl.select();
                 };
 
                 scope.$on('setInputReady', select);
             },
-            template: '<textarea ng-model="input"></textarea>'
+            template: '<textarea ng-model="input" ng-style="{ top: px(editorTopPosition()), left: px(editorLeftPosition()), position: textAreaPosition}"></textarea>'
         };
 
     }]);
