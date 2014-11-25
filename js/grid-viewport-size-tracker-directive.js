18229dd898ebcff58091f577b2974df1535c88c1
Anton Nekipelov <anton.nekipelov@gmail.com>
2014-11-25 10:54:10 +0100

    fix the typo in method name

--- js/grid-viewport-size-tracker-directive.js
+++ js/grid-viewport-size-tracker-directive.js
@@ -7,12 +7,12 @@
             restrict: 'A',
             link: function (scope) {
 
-                var broadCastResize = _.debounce(function () {
+                var broadcastResize = _.debounce(function () {
                     scope.$broadcast('grid-viewport-size-tracker.resize');
                 }, 250);
 
                 window.addEventListener('resize', function () {
-                    broadCastResize();
+                    broadcastResize();
                 });
 
             }
