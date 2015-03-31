ae8ad0e8198f58b8e4a2857580e542e350876821
Anton Nekipelov <anton.nekipelov@gmail.com>
2015-03-31 15:16:44 +0200

    trim the last empty row from pasted text

--- build/build.js
+++ build/build.js
@@ -2094,7 +2094,14 @@ window.angular.module('connect-grid', []);
                 return String(text).indexOf('\n') > -1 || String(text).indexOf('\t') > -1;
             },
             getRows: function (text) {
-                return String(text).split('\n');
+                var result = String(text).split('\n');
+
+                //remove last row, if it is empty
+                if (result.length > 0 && String(result[result.length - 1]).trim().length === 0) {
+                    result.pop();
+                }
+
+                return result;
             },
             getColumns: function (text) {
                 return String(text).split('\t');
--- js/grid-input-parser-service.js
+++ js/grid-input-parser-service.js
@@ -8,7 +8,14 @@
                 return String(text).indexOf('\n') > -1 || String(text).indexOf('\t') > -1;
             },
             getRows: function (text) {
-                return String(text).split('\n');
+                var result = String(text).split('\n');
+
+                //remove last row, if it is empty
+                if (result.length > 0 && String(result[result.length - 1]).trim().length === 0) {
+                    result.pop();
+                }
+
+                return result;
             },
             getColumns: function (text) {
                 return String(text).split('\t');
